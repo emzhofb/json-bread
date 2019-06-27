c680ea0af48479c45ee90296727c62565ea68c37
ikhda <ikhdamuhammad@gmail.com>
2019-06-27 16:10:05 +0700

    mockup bread

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ app.set('view engine', 'ejs'); // register the template engine
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', (req, res) => res.render('index'));
+app.get('/add', (req, res) => res.render('add'));
+app.get('/edit', (req, res) => res.render('edit'));
 
 app.listen(port, () => console.log(`json-bread listening on port ${port}!`));
