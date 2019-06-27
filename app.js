7368c733b84778531477dce978aed086683afd64
ikhda <ikhdamuhammad@gmail.com>
2019-06-27 15:01:15 +0700

    mockup bread

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'html');
+app.set('views', './views'); // specify the views directory
+app.set('view engine', 'ejs'); // register the template engine
 
 app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(port, () => console.log(`json-bread listening on port ${port}!`));
